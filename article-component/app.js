2d104b996f27e1833910a1554be45b689ab0d8ae
LeoZion <leozionsuporte@gmail.com>
2021-09-20 17:25:19 -0300

    Js functions

--- article-component/app.js
+++ article-component/app.js
@@ -1,9 +1,25 @@
 // store ".share" & ".share-img" in variables
-
+const share = document.querySelector('.share');
+const shareIcon = document.querySelector('.share-img');
+const shareClose = document.querySelector('.share-mobile-close');
 
 // listen for click on icon
-
+shareIcon.addEventListener('click', (e) => {
   // Within listner
   // show/hide correct share menu
   // avoid shutting the share popup when clicking on it
 
+  if (e.target === shareIcon) {
+    if (window.innerWidth <= 750) {
+      share.classList.toggle('active-share-mobile');
+      return;
+    }
+
+    share.classList.toggle('active-share-desktop');
+  }
+});
+
+shareClose.addEventListener('click', () => {
+  share.classList.toggle('active-share-mobile');
+  return;
+});
